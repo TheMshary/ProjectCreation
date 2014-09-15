d36afcc6001b0ef3610b03d205532feb3e8b2320
apple <apple@apples-MacBook-Air.local>
2014-09-15 21:10:10 +0300

    new js code

--- static/main.js
+++ static/main.js
@@ -1,3 +1,9 @@
-function buy(element) {
-	document.getElementById(element).
-}
+$(function() {
+
+	$(".buy").on("click", function() {
+		// Get #element value in inventory, parse to int, increment, print.
+		var fire = parseInt($("#fire").text());
+		$("#fire").html("<b>"+(fire+1)+"</b>");
+	});
+
+});
