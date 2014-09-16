28a7b0859e12bc02000bc6b9639688fec5bc12e0
apple <apple@apples-MacBook-Air.local>
2014-09-16 21:32:39 +0300

    some commit

--- static/main.js
+++ static/main.js
@@ -1,9 +1,30 @@
 $(function() {
 
 	$(".buy").on("click", function() {
-		// Get #element value in inventory, parse to int, increment, print.
-		var fire = parseInt($("#fire").text());
-		$("#fire").html("<b>"+(fire+1)+"</b>");
+		var $id = $(this).attr("id");
+		var $b = $("td[id="+$id+"]").text();
+		var $q = parseInt($b);
+		$("#"+$id).html("<b>"+($q+1)+"</b>");
 	});
+
+	/*function buy_item() {
+		var $id = $(this).attr("id");
+		var $b = $("td[id="+$id+"]").text();
+		var $q = parseInt($b);
+		$($b).html("<b>"+($q+1)+"</b>");
+
+	}*/
+
+	function add_checked_box() {
+		$("input[class=invcb]").on("click", function() {
+
+		});
+
+	}
+
+	function combine(elo, elt) {
+		var $fire = $("Firecb")
+
+	}
 
 });
